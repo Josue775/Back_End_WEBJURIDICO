d5c5ac6aa10923edf0b1e3b2f82589f8f765bf76
Josue775 <125761462+Josue775@users.noreply.github.com>
2024-05-31 10:16:33 -0600

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,11 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv'; // Importa el paquete dotenv para cargar las variables de entorno
 dotenv.config(); 
-export const sequelize = new Sequelize('juridicodb', 'postgres', '12345', {
-  host: 'localhost',
-  dialect: 'postgres' 
+export const sequelize = new Sequelize(process.env.URLDB, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
